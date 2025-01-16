24720a169c1b1f8f3c77274bc5ab9b8ae5225b0c
interstellar-tars <140336264+interstellar-tars@users.noreply.github.com>
2025-01-16 20:52:41 +0000

    Create avatar.js

---
+++ game/avatar.js
@@ -0,0 +1,10 @@
+class Avatar {
+    constructor(data) {
+        this.data = data;
+    }
+
+    render(gl) {
+        console.log("Rendering avatar with WebGL...");
+        // WebGL rendering logic here
+    }
+}
